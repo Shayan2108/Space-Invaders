51435cbe15aaf393b19f86ee1058468159721b9f
Shayan2108 <mohammadmubashar1@gmail.com>
2026-01-02 19:00:32 +0100

    update per far runnare il gioco

---
+++ Explosion.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Explosion {
+
+    public boolean finita;
+
+    public Explosion(int x, int y, BufferedImage[] framesEsplosione) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void aggiorna() {
+        
+    }
+
+    public void disegna(Graphics g) {
+        
+    }
+    
+}
