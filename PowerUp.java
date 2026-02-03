c255ec8b679abdd6b5795bb3f82b09efc79a556b
gito08togi <crigiu84@gmail.com>
2026-02-03 19:05:34 +0100

    Revert "modifica della logica dei power up (incompleto ed errato, da modificare in seguito)"
    This reverts commit 45015c6d0d0cc0c73d86928c67ee6ea6b76537a3.

--- PowerUp.java
+++ PowerUp.java
@@ -1,88 +1,42 @@
 import java.awt.Graphics;
-import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
-public class PowerUp extends Pianeti {
-
+public class PowerUp extends Thread {
     public enum Tipo {
         MULTI, SCUDO, RESPINGI, RALLENTA, PENETRANTE
     }
 
+    public int x, y;
+    public int velocita;
+    public MyPanel m;
+    public BufferedImage image;
     public Tipo tipo;
 
-    public PowerUp(int x, int y, int velocita, MyPanel m, Tipo tipo) throws IOException {
-        super(x, y, velocita, m, caricaImmagini(tipo));
-        this.tipo = tipo;
-        this.grandezzaPianeta = 50; // dimensione fissa dei power-up
+    public PowerUp(int x, int y, int velocita, MyPanel m, BufferedImage image, Tipo tipo) {
+        this.x = x;
+        this.y = y;
         this.velocita = velocita;
+        this.m = m;
+        this.image = image;
+        this.tipo = tipo;
     }
 
-    // Carica immagini in base al tipo di power-up
-    private static ArrayList<BufferedImage> caricaImmagini(Tipo tipo) {
-        ArrayList<BufferedImage> imgs = new ArrayList<>();
-        String cartella = "PowerUps/";
-
-        switch (tipo) {
-            case MULTI -> cartella += "MULTI/";
-            case SCUDO -> cartella += "SCUDO/";
-            case RESPINGI -> cartella += "RESPINGI/";
-            case RALLENTA -> cartella += "RALLENTA/";
-            case PENETRANTE -> cartella += "PENETRANTE/";
-        }
-
-        for (int i = 0;; i++) {
-            try {
-                imgs.add(ImageIO.read(new File(cartella + i + ".png")));
-            } catch (IOException e) {
-                break; // esce quando non ci sono più immagini
-            }
-        }
-        return imgs;
-    }
-
-    // Collisione con la nave
-    public boolean collideConNave(MyPanel m) {
-        Rectangle nave = new Rectangle(m.xNave, m.yNave, m.larghezzaNave, m.altezzaNave);
-        Rectangle box = new Rectangle(x, y, grandezzaPianeta, grandezzaPianeta);
-        return nave.intersects(box);
-    }
-
-    // Applica l'effetto del power-up
-    public void applicaPowerUp(MyPanel m) {
-        switch (tipo) {
-            case MULTI -> {
-                m.sparoMultiplo = true;
-                m.timerMulti = System.currentTimeMillis() + 5000;
-            }
-            case SCUDO -> {
-                m.scudoAttivo = true;
-                m.timerScudo = System.currentTimeMillis() + 5000;
-            }
-            case PENETRANTE -> {
-                m.proiettiliPenetranti = true;
-                m.timerPenetrante = System.currentTimeMillis() + 5000;
-            }
-            case RALLENTA -> {
-                m.velocitaNemici = m.velocitaNemiciBase / 2;
-                m.timerRallenta = System.currentTimeMillis() + 5000;
-            }
-            case RESPINGI -> {
-                for (Nemico n : m.nemici) {
-                    n.y -= 50;
-                    if (n.y < 0)
-                        n.y = 0;
-                }
-            }
-        }
-    }
-
-    // Disegna il power-up
     @Override
+    public void run() {
+        while (y <= m.getHeight()) {
+            y += velocita;
+            try {
+                sleep(33);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // synchronized (m.powerUps) {
+        //     m.powerUps.remove(this);
+        // }
+    }
+
     public void stampaOggettiClasse(Graphics g) {
-        super.stampaOggettiClasse(g);
+        g.drawImage(image, x, y, 50, 50, null); // dimensione standard
     }
 }
--- SpostaPowerUp.java
+++ SpostaPowerUp.java
@@ -1,121 +1,55 @@
-import java.awt.Rectangle;
-import java.io.File;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-
-public class SpostaPowerUp extends Thread {
-
+class SpostaPowerUp extends Thread {
     MyPanel m;
 
     public SpostaPowerUp(MyPanel m) {
         this.m = m;
-        setName("PowerUpThread");
     }
 
-    @Override
-    public void run() {
-        while (!m.gameOver) {
+    //@Override
+    // public void run() {
+    //     while (!m.gameOver) {
+    //         synchronized (m.powerUps) {
+    //             for (int i = 0; i < m.powerUps.size(); i++) {
+    //                 PowerUp p = m.powerUps.get(i);
 
-            synchronized (m.powerUps) {
-                for (int i = 0; i < m.powerUps.size(); i++) {
-                    PowerUp p = m.powerUps.get(i);
-                    p.sposta();
+    //                 // muovi il power-up
+    //                 p.y += p.velocita;
 
-                    Rectangle nave = new Rectangle(
-                            m.xNave, m.yNave,
-                            m.larghezzaNave, m.altezzaNave);
+    //                 // collisione con la nave
+    //                 Rectangle navBox = new Rectangle(m.xNave, m.yNave, m.larghezzaNave, m.altezzaNave);
+    //                 Rectangle powerBox = new Rectangle(p.x, p.y, 50, 50); // dimensione fissa
 
-                    Rectangle box = new Rectangle(p.x, p.y, 40, 40);
+    //                 if (navBox.intersects(powerBox)) {
+    //                     applicaEffetto(p);
+    //                     m.powerUps.remove(i);
+    //                     i--;
+    //                     continue;
+    //                 }
 
-                    if (nave.intersects(box)) {
-                        applicaPowerUp(p);
-                        m.powerUps.remove(i);
-                        i--;
-                        continue;
-                    }
+    //                 // rimuovi se fuori schermo
+    //                 if (p.y > m.getHeight()) {
+    //                     m.powerUps.remove(i);
+    //                     i--;
+    //                 }
+    //             }
+    //         }
 
-                    if (p.y > m.getHeight()) {
-                        m.powerUps.remove(i);
-                        i--;
-                    }
-                }
-            }
+    //         try {
+    //             sleep(33);
+    //         } catch (InterruptedException e) {
+    //             e.printStackTrace();
+    //         }
+    //     }
+    // }
 
-            gestisciTimer();
-
-            try {
-                sleep(33);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void applicaPowerUp(PowerUp p) {
-        switch (p.tipo) {
-
-            case MULTI -> {
-                m.sparoMultiplo = true;
-                m.timerMulti = System.currentTimeMillis() + 5000;
-            }
-
-            case SCUDO -> {
-                m.scudoAttivo = true;
-                m.timerScudo = System.currentTimeMillis() + 5000;
-            }
-
-            case PENETRANTE -> {
-                m.proiettiliPenetranti = true;
-                m.timerPenetrante = System.currentTimeMillis() + 5000;
-            }
-
-            case RALLENTA -> {
-                m.velocitaNemici = m.velocitaNemiciBase / 2;
-                m.timerRallenta = System.currentTimeMillis() + 5000;
-            }
-
-            case RESPINGI -> {
-                for (Nemico n : m.nemici) {
-                    n.y -= 50; // li spinge verso l'alto
-                    if (n.y < 0)
-                        n.y = 0;
-                }
-            }
-        }
-        /*
-         * try {
-         * AudioInputStream audio = AudioSystem.getAudioInputStream(new
-         * File("powerup.wav"));
-         * Clip clip = AudioSystem.getClip();
-         * clip.open(audio);
-         * clip.start();
-         * } catch (Exception e) {
-         * e.printStackTrace();
-         * }
-         */
-
-    }
-
-    private void gestisciTimer() {
-        long now = System.currentTimeMillis();
-
-        if (m.sparoMultiplo && now > m.timerMulti) {
-            m.sparoMultiplo = false;
-        }
-
-        if (m.scudoAttivo && now > m.timerScudo) {
-            m.scudoAttivo = false;
-        }
-
-        if (m.proiettiliPenetranti && now > m.timerPenetrante) {
-            m.proiettiliPenetranti = false;
-        }
-
-        if (m.timerRallenta > 0 && System.currentTimeMillis() > m.timerRallenta) {
-            m.velocitaNemici = m.velocitaNemiciBase;
-            m.timerRallenta = 0;
-        }
-    }
+    // private void applicaEffetto(PowerUp p) {
+    //     switch (p.tipo) {
+    //         case MULTI -> m.attivaSparoMultiplo();
+    //         case SCUDO -> m.attivaScudo();
+    //         case RESPINGI -> m.attivaRespingi();
+    //         case RALLENTA -> m.attivaRallenta();
+    //         case PENETRANTE -> m.attivaProiettiliPenetranti();
+    //     }
+    // }
 }
