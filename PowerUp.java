caa2c441e6dd291467597f8e970e1715d8ea12ba
Shayan2108 <mohammadmubashar1@gmail.com>
2026-02-06 10:49:28 +0100

    documentazione

--- PowerUp.java
+++ PowerUp.java
@@ -1,25 +1,46 @@
-import java.awt.Graphics;
-import java.awt.Rectangle;
+
+/**
+ * @file PowerUp.java
+ *
+ * @author Mohammad Shayan Attari Bin Mohammad Zeshan Attari
+ * @version 1.0
+ *
+ * @brief Classe PowerUp
+ *
+ * Rappresenta un power-up che scende dall’alto.
+ * Quando viene preso, attiva un effetto temporaneo.
+ */
+
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class PowerUp extends Pianeti {
+
     int tipo;
     Rectangle hitbox;
+
     volatile boolean effettoFinito;
     volatile boolean effettoIniziato;
     boolean iniziatoUnaVolta;
     volatile boolean finireThread;
+
     Long timerPowerUp;
 
-    public PowerUp(int x, int y, int velocita, MyPanel m, ArrayList<BufferedImage> image, int tipo) {
+    public PowerUp(int x, int y, int velocita, MyPanel m,
+            ArrayList<BufferedImage> image, int tipo) {
+
         super(x, y, velocita, m, image);
+
         super.x = x;
         super.y = y;
         super.velocita = 5;
+
         maxFrame = 60;
         this.tipo = tipo;
+
         hitbox = new Rectangle(x, y, 25, 25);
+
         effettoFinito = false;
         effettoIniziato = false;
         finireThread = false;
@@ -28,39 +49,57 @@ public class PowerUp extends Pianeti {
 
     @Override
     public void run() {
-        while ((y <= m.getHeight()|| iniziatoUnaVolta) && !finireThread && !m.gameOver) {
+
+        while ((y <= m.getHeight() || iniziatoUnaVolta)
+                && !finireThread && !m.gameOver) {
+
             y += velocita;
             hitbox.translate(0, velocita);
+
             try {
                 sleep(33);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
             if (effettoIniziato) {
+
                 if (tipo == 1) {
                     m.bulletMassime += 16;
                     timerPowerUp = System.currentTimeMillis() + 10000;
                 }
+
                 effettoIniziato = false;
                 iniziatoUnaVolta = true;
             }
-            if(timerPowerUp != null && timerPowerUp < System.currentTimeMillis()) {
+
+            if (timerPowerUp != null &&
+                    timerPowerUp < System.currentTimeMillis()) {
+
                 if (tipo == 1) {
                     m.bulletMassime -= 16;
                 }
+
                 finireThread = true;
             }
         }
+
         synchronized (m.powerUps) {
             m.powerUps.remove(this);
         }
     }
 
     public void stampaOggettiClasse(Graphics g) {
-        g.drawImage(image.get(frameAttuale), x, y, 25, 25, null); // dimensione standard
+
+        g.drawImage(
+                image.get(frameAttuale),
+                x, y, 25, 25,
+                null);
+
         if (System.currentTimeMillis() >= timer) {
             frameAttuale++;
             timer += frequenzaAggiornamento;
+
             if (frameAttuale >= maxFrame)
                 frameAttuale = 0;
         }
