411bd709379c592814cd4a73f323edb81f52dd9a
gito08togi <crigiu84@gmail.com>
2026-02-08 23:53:15 +0100

    toglimento della parte del volume che è troppo difficile da fare (ho provato a fare in modo che dal menu di pausa pitevi abbassare o alzare il volume, ma ogni cavolo di modifica che faccio si bugga tutto sto gioco)

--- PauseMenuDialog.java
+++ PauseMenuDialog.java
@@ -3,8 +3,6 @@ import java.awt.*;
 
 public class PauseMenuDialog extends JDialog {
 
-    private JSlider volumeSlider;
-    private JLabel volumeLabel;
     private MyPanel gamePanel;
 
     public PauseMenuDialog(Window owner, MyPanel gamePanel) {
@@ -16,39 +14,24 @@ public class PauseMenuDialog extends JDialog {
         setLayout(null);
         getContentPane().setBackground(new Color(0, 0, 0, 200));
 
-        // 🔹 Torna al gioco
+        // Torna al gioco
         JButton resumeBtn = new JButton("Torna al gioco");
-        resumeBtn.setBounds(120, 60, 160, 40);
+        resumeBtn.setBounds(120, 80, 160, 40);
         resumeBtn.addActionListener(e -> {
             gamePanel.isPaused = false;
             dispose();
         });
         add(resumeBtn);
 
-        // 🔹 Esci dal gioco
+        // Esci dal gioco
         JButton quitBtn = new JButton("Esci dalla partita");
-        quitBtn.setBounds(120, 120, 160, 40);
+        quitBtn.setBounds(120, 150, 160, 40);
         quitBtn.addActionListener(e -> {
             JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(gamePanel);
             frame.dispose();
             System.exit(0);
         });
         add(quitBtn);
-
-        // 🔹 Volume
-        volumeLabel = new JLabel("Volume: " + gamePanel.getVolume() + "%");
-        volumeLabel.setForeground(Color.WHITE);
-        volumeLabel.setBounds(150, 180, 200, 20);
-        add(volumeLabel);
-
-        volumeSlider = new JSlider(0, 100, gamePanel.getVolume());
-        volumeSlider.setBounds(100, 210, 200, 40);
-        volumeSlider.addChangeListener(e -> {
-            int vol = volumeSlider.getValue();
-            gamePanel.setVolume(vol);
-            volumeLabel.setText("Volume: " + vol + "%");
-        });
-        add(volumeSlider);
 
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
     }
